bde43ef7d78149ac090db0ed5eb99c1283bad979
mauromamani <mauromamani.b@gmail.com>
2022-02-18 13:58:00 -0300

    project structure

---
+++ cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting server on :5000")
+	err := http.ListenAndServe(":5000", mux)
+	log.Fatal(err)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitlab.com/mauromamani20014/snippetbox
+
+go 1.17
