8063862f55f18db1835846b4e174493530768adc
mauromamani <mauromamani.b@gmail.com>
2022-02-28 23:24:22 -0300

    chapter 8.0 create page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -63,23 +63,5 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
-
-	// dummy data
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := "7"
-
-	// insert method
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	app.render(w, r, "create.page.tmpl", nil)
 }
