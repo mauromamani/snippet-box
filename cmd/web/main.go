e0e090d4c3003abed7cabc17010e508d826badc6
mauromamani <mauromamani.b@gmail.com>
2022-03-05 20:33:41 -0300

    chapter 13.4 mocking dependencies

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,7 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golangcollege/sessions"
+	"gitlab.com/mauromamani20014/snippetbox/pkg/models"
 	"gitlab.com/mauromamani20014/snippetbox/pkg/models/mysql"
 )
 
@@ -21,12 +22,20 @@ const contextKeyIsAuthenticated = contextKey("isAuthenticated")
 
 // define app struct
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	session       *sessions.Session
-	snippets      *mysql.SnippetModel
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	session  *sessions.Session
+	snippets interface {
+		Insert(string, string, string) (int, error)
+		Get(int) (*models.Snippet, error)
+		Latest() ([]*models.Snippet, error)
+	}
 	templateCache map[string]*template.Template
-	users         *mysql.UserModel
+	users         interface {
+		Insert(string, string, string) error
+		Authenticate(string, string) (int, error)
+		Get(int) (*models.User, error)
+	}
 }
 
 func main() {
